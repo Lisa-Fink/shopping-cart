20eb14598863a7b0f9087d0eb824448a778c24fb
Lisa Fink <lisadfink@gmail.com>
2022-06-14 13:42:04 -0400

    Added components to page with routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Nav from './components/Nav';
 
 function App() {
-  return <div></div>;
+  const [cart, setCart] = useState([]);
+  return (
+    <BrowserRouter>
+      <Nav cart={cart} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
