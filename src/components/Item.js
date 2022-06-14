e9de8e849a1179f825ae43c30eef18c18b018d73
Lisa Fink <lisadfink@gmail.com>
2022-06-14 15:04:13 -0400

    Changed quantity input to only accept numbers

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,17 +6,19 @@ const Item = (props) => {
   const [quantity, setQuantity] = useState(1);
 
   const addToCart = () => {
-    if (!props.cart.filter((item) => item.key === props.item.key).length) {
-      props.item.quantity = quantity;
-      props.setCart([...props.cart, props.item]);
-    } else {
-      const newCart = props.cart.map((item) => {
-        if (item.key === props.item.key) {
-          item.quantity += quantity;
-        }
-        return item;
-      });
-      props.setCart(newCart);
+    if (quantity) {
+      if (!props.cart.filter((item) => item.key === props.item.key).length) {
+        props.item.quantity = quantity;
+        props.setCart([...props.cart, props.item]);
+      } else {
+        const newCart = props.cart.map((item) => {
+          if (item.key === props.item.key) {
+            item.quantity += quantity;
+          }
+          return item;
+        });
+        props.setCart(newCart);
+      }
     }
   };
 
@@ -33,7 +35,11 @@ const Item = (props) => {
           className="quantity-inpt"
           type="text"
           value={quantity}
-          onChange={(e) => setQuantity(Number(e.target.value))}
+          onChange={(e) =>
+            e.target.value.match(/\d+/) | (e.target.value === '')
+              ? setQuantity(Number(e.target.value))
+              : quantity
+          }
         />
         <div className="arrows">
           <span
@@ -45,7 +51,7 @@ const Item = (props) => {
           </span>
           <span
             onClick={() => {
-              if (quantity !== 1) {
+              if (quantity > 1) {
                 setQuantity(quantity - 1);
               }
             }}
