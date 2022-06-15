62b42fc0e66c0d51df2bbc987aefdf5bd160fff2
Lisa Fink <lisadfink@gmail.com>
2022-06-14 23:03:45 -0400

    Moved cart size to App

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import { useState } from 'react';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Nav from './components/Nav';
+import Cart from './components/Cart';
 
 function App() {
   const [cart, setCart] = useState([]);
+  const cartSize = cart.reduce((prev, current) => {
+    return prev + current.quantity;
+  }, 0);
   return (
     <BrowserRouter>
-      <Nav cart={cart} />
+      <Nav cartSize={cartSize} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
+        <Route
+          path="/cart"
+          element={<Cart cart={cart} setCart={setCart} cartSize={cartSize} />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,18 +2,16 @@ import React from 'react';
 import '../styles/Nav.css';
 import { Link } from 'react-router-dom';
 
-const Nav = (props) => {
-  const cart = props.cart;
-  const cartSize = cart.reduce((prev, current) => {
-    return prev + current.quantity;
-  }, 0);
+const Nav = ({ cartSize }) => {
   return (
     <nav>
       <h1>FakeStore</h1>
       <div id="links">
         <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
-        <button>Cart {cartSize}</button>
+        <Link to="/cart">
+          <button>Cart {cartSize}</button>
+        </Link>
       </div>
     </nav>
   );
